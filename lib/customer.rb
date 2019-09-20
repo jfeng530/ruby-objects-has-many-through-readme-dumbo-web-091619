8267cf73edaca3588fd05729d2a145a98f64b090
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-20 01:38:30 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
 class Customer
   
+  attr_accessor :name, :assert_generates
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
